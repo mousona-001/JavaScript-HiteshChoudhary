8689bc21ffea4e88cb741dcea47b4ab9346e7ead
Mousona Acharjee <109936969+mousona-001@users.noreply.github.com>
2023-07-22 17:44:14 +0000

    Datatypes

---
+++ 03_basics/test3.js
@@ -0,0 +1,19 @@
+//"use strict"; //treat all js code as newer version
+
+// alert(3+3);    // will give error as we use node js, not browser
+
+let age = 18;
+let adult = true;
+let name = null;
+
+/*
+Datatypes : 
+number => 
+boolean => true or false
+null => standalone value
+undefined => value not assigned yet
+symbol => uniqueness 
+*/
+
+console.log(typeof null);   // type of null is object
+console.log(typeof undefined);  // type of undefined is undefined 
