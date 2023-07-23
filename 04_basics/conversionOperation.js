69c774465ea44069011b8d2e852e1efa1dad385a
Mousona Acharjee <109936969+mousona-001@users.noreply.github.com>
2023-07-23 16:36:58 +0000

    Conversion Operation

---
+++ 04_basics/conversionOperation.js
@@ -0,0 +1,44 @@
+// let score = 33;
+// console.log(typeof score);
+
+// let stringValue = "33";
+// console.log(typeof stringValue);
+``
+// let score = null;`
+// console.log(typeof score);
+
+// type conversion --->
+
+// let valueChange = Number(score)
+// console.log(typeof valueChange)
+// console.log(valueChange);
+
+// let valueInNumber = Number(stringValue);    // string value changes to integer value;
+// console.log(typeof valueInNumber);
+
+let isLoggedInt = 1;
+let booleanLoggedIn = Boolean(isLoggedInt);
+console.log(booleanLoggedIn);
+console.log(typeof booleanLoggedIn);
+
+
+/*
+    Investigation/Summary ---->
+
+    33 => Number
+    "33" => type is String
+    1 => true
+    0 => false
+    "mou" => true
+    " " => false
+    "33abc" => NaN
+    typeof => use for type conversion
+    "33" => 33    // string to number type conversion
+*/
+
+console.log(1 + 1);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "3");
+console.log("1" + 2 + 3);
+
